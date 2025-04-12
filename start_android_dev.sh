447e74a557a76f99cf9e7cc5526c30dd6323dd23
Syver Stensholt <stonegrove@duck.com>
2025-04-12 22:57:16 +0200

    add start script

---
+++ start_android_dev.sh
@@ -0,0 +1,4 @@
+export NDK_HOME=/home/syvers/Android/Sdk/ndk/28.0.13004108
+export ANDROID_HOME=/home/syvers/Android/Sdk
+export JAVA_HOME=/home/syvers/.local/share/JetBrains/Toolbox/apps/android-studio/jbr
+bun tauri android dev
